a9b4360a45684460f8981e23e567018b558309f0
Alex Jung <aevum20@gmail.com>
2024-06-07 16:20:10 +0900

    Strand Card Structure

--- lib/actions/strand.actions.ts
+++ lib/actions/strand.actions.ts
@@ -33,3 +33,71 @@ export async function createStrand({
 
   revalidatePath(path);
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  // Calculate the number of posts to skip
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  // Fetch the posts that have no parents (top-level strands)
+  const postsQuery = Strand.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+
+  const totalPostsCount = await Strand.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const posts = await postsQuery.exec();
+
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
+}
+
+export async function fetchStrandById(id: string) {
+  try {
+    // TODO: Populate community
+    const strand = await Strand.findById(id)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      })
+      .populate({
+        path: "children",
+        populate: [
+          {
+            path: "author",
+            model: User,
+            select: "_id id name parentId image",
+          },
+          {
+            path: "children",
+            model: Strand,
+            populate: {
+              path: "author",
+              model: User,
+              select: ")id id name parentId image",
+            },
+          },
+        ],
+      })
+      .exec();
+
+    return strand;
+  } catch (error: any) {
+    throw new Error(`Error fetching strand: ${error.message}`);
+  }
+}
